410054ed5049a7e7273c3b4506f8eba44b0e0272
Luiz Guilherme Martinho Sampaio Ito <41489663+Guiimartinho@users.noreply.github.com>
2020-06-23 21:36:14 -0300

    Update Bateria_Voltimetro.ino

--- Bateria_Voltimetro.ino
+++ Bateria_Voltimetro.ino
@@ -1,19 +1,19 @@
 /**
  * Nome: Luiz Guilherme M. S. Ito
- * Voltmetro
- * Date: Jun a, 2020
+ * Voltimetro
+ * Date: Jun 1, 2020
  *
- * NOTE: O Voltómetro só é capaz de medir tensões de 0 - 5V.
+ * NOTE: O Voltímetro só é capaz de medir tensões de 0 - 5V.
  * Para tensões maiores, utilizar módulos divisores de Tensão para 5V de saída
- * L
+ * 
  */
 
 #include <LiquidCrystal_I2C.h>
 
-// Constantes
+// Constants
 int VOLTAGE_PIN = A0;
 
-// Variáveis Globais
+// Global variables
 LiquidCrystal_I2C lcd(0x27,16,2);
 
 // ----- Bits para Caracteres Especiais para Bateria -----
@@ -79,6 +79,16 @@ byte bat5[8] = {
 };
 byte bat6[8] = {
   B01110,
+  B11011,
+  B11111,
+  B11111,
+  B11111,
+  B11111,
+  B11111,
+  B11111
+};
+byte bat7[8] = {
+  B01110,
   B11111,
   B11111,
   B11111,
@@ -90,7 +100,7 @@ byte bat6[8] = {
 
 void setup() {
 
-  // Inicialização do LCD e limpeza
+  // Initialize the LCD and clear it
   lcd.begin(16, 2);
   lcd.clear();
 
@@ -101,6 +111,7 @@ void setup() {
   lcd.createChar(4, bat4);
   lcd.createChar(5, bat5);
   lcd.createChar(6, bat6);
+  lcd.createChar(7, bat7);
   lcd.setCursor(15, 0);
 }
 
@@ -118,16 +129,21 @@ void loop() {
 
   porcent = map(sensorValue, 658.9, 1023, 0, 100);
 
+   if(porcent > 98.1){
+    lcd.setCursor(15, 0);
+    lcd.write(7);
+    }
  
-  if(porcent > 92.0){
+  else if((porcent > 91.5) && (porcent < 98.0)){
     // Display the voltage on the LCD
   lcd.setCursor(15, 0);
   //lcd.print(porcent);
   //lcd.print("%");
   lcd.write(6);
+  //lcd.clear();
   }
 
-  else if((porcent > 75.3) && (porcent < 91.9)){
+  else if((porcent > 75.3) && (porcent < 91.4)){
     lcd.setCursor(15, 0);
     lcd.write(5);
     }
@@ -135,30 +151,33 @@ void loop() {
      else if((porcent > 58.6) && (porcent < 75.2)){
     lcd.setCursor(15, 0);
     lcd.write(4);
+    //lcd.clear();
     }
 
      else if((porcent > 41.9) && (porcent < 58.5)){
     lcd.setCursor(15, 0);
     lcd.write(3);
+    //lcd.clear();
     }
 
      else if((porcent > 25.2) && (porcent < 41.8)){
     lcd.setCursor(15, 0);
     lcd.write(2);
+    //lcd.clear();
     }
 
      else if((porcent > 8.5) && (porcent < 25.1)){
     lcd.setCursor(15, 0);
     lcd.write(1);
+    //lcd.clear();
     }
     
      else if((porcent < 8.49)){
     lcd.setCursor(15, 0);
     lcd.write(0);
+    //lcd.clear();
     }
-     
-  
-
-  // Wait 200 ms before taking another reading
+     // Wait 200 ms before taking another reading
   delay(200);
+ 
 }
